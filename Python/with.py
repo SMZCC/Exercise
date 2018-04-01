573fe6c5ef77894e9f0e6f13cda1aa04a3a1c21c
SMZCC <2320966859@qq.com>
2018-04-01 16:20:21 +0800

    with 不会释放变量

---
+++ Python/with.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# date: 2018-4-1,16:16:24
+# name: smz
+
+
+import numpy as np
+
+
+if __name__ == '__main__':
+
+    with open('/media/smz/SMZ_WORKING_SPACE/Github/Exercise/labels/test_labels.txt') as f:
+        data = f.read()
+
+    print 'hello with'
+    print data   # 变量值依旧存在,没有被释放
