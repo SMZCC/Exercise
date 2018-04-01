90b6744c02af412e2f9d68076e5fe9e6b087f680
SMZCC <2320966859@qq.com>
2018-04-01 16:15:34 +0800

    tf_variable_scope parameters

---
+++ TF/tf_variable.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+# date: 2018-3-31,20:43:06
+# name: smz
+
+
+import tensorflow as tf
+
+def main():
+    sess = tf.InteractiveSession()
+    with tf.variable_scope('scope_1', 'defaut_scope') as sc:
+        print 'scope_1:', sc.name   #
+
+    with tf.variable_scope('default_scope') as sc:
+        print 'default_scope:', sc.name
+
+    # with tf.variable_scope(default_name='test_default_name') as sc:
+    #     print 'test_default_name:', sc.name  # 这里位置参数不可省,故而这种跳过位置参数来给关键字
+    #                                          # 赋值的方式是不行的
+
+if __name__ == '__main__':
+    main()
