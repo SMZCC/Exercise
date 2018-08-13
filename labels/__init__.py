6aca68cb9ab75c589022d97626ba93a267f3d841
SMZCC <2320966859@qq.com>
2018-08-13 20:35:58 +0800

    py_decorator

--- labels/__init__.py
+++
@@ -1,3 +0,0 @@
-# coding=utf-8
-# date:2018-4-26,19:17:53
-# name: smz
--- labels/options.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-# date: 2018-3-13,14:58:09
-# name: smz
-
-
-INPUT_DIR = '/media/smz/Apps/UbuntuVirtualBoxUsing/file_sharing/train'
-SAVE_DIR = './train_labels.txt'
